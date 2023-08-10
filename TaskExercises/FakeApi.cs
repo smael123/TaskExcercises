cfcf23afe37491fbab54446ec3991001919e3f16
Ismael Almaguer <ismael.alma@gmail.com>
2023-08-10 00:36:15 -0500

    first commit

---
+++ TaskExercises/FakeApi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace TaskExercises
+{
+	public class FakeApi
+	{
+		private readonly string[] _peopleNames;
+		private readonly Dictionary<string, int> _salaryDictionary;
+
+		public FakeApi()
+		{
+			_peopleNames = new string[] { "Adam", "Eve", "John" };
+
+			_salaryDictionary = new()
+			{
+				{ "Adam", 100 },
+                { "Eve", 150 },
+                { "John", 200 },
+            };
+        }
+
+		public void GetSalaryByName(string name, ConcurrentBag<(string, int)> salaryList, int sleepTime = 0, bool invokeError = false)
+		{
+			_salaryDictionary.TryGetValue(name, out int salary);
+
+			Thread.Sleep(TimeSpan.FromSeconds(sleepTime));
+
+            if (invokeError)
+            {
+                throw new Exception($"{nameof(GetSalaryByName)} Error. Person Name: {name}");
+            }
+
+            salaryList.Add((name, salary));
+		}
+    }
+}
+
---
+++ TaskExercises/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace TaskExercises;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            //when all example with shared results
+            FakeApi fakeApi = new();
+
+            ConcurrentBag<(string, int)> salaryList = new();
+
+            Task[] tasks = new[] {
+                Task.Run(() => fakeApi.GetSalaryByName("Adam", salaryList)), //task items will start running even before you await WhenAll
+                Task.Run(() => fakeApi.GetSalaryByName("Eve", salaryList, 10)),
+                Task.Run(() => fakeApi.GetSalaryByName("John", salaryList, 1, true))
+            };
+            var whenAllTask = Task.WhenAll(tasks);
+
+            try
+            {
+                await whenAllTask;
+            }
+            catch { } //we want to eat the exception so that we can report that some of the tasks failed
+
+            switch (whenAllTask.Status)
+            {
+                case TaskStatus.RanToCompletion:
+                    Console.WriteLine("Found everyone, Total Salary: {0}", salaryList.Sum(c => c.Item2));
+                    break;
+                case TaskStatus.Faulted:
+                    Console.WriteLine(
+                        "Some failed, Total Salary: {0} Inner Exception(s): {1}"
+                        ,salaryList.Sum(c => c.Item2), string.Join(" | "
+                        ,whenAllTask.Exception?.InnerExceptions.Select(ie => ie.Message) ?? Array.Empty<string>()) //the exceptions that the task item methods throw are inner
+                    );
+                    break;
+                default:
+                    Console.WriteLine("Something else happened, Total Salary: {0}", salaryList.Sum(c => c.Item2));
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Main Error: {ex.Message}");
+        }
+        
+        Console.ReadKey();
+    }
+}
